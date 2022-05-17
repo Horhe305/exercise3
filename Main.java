a6ab67b8c388ef25711a790141d3cb1522fc81fa
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-05-17 11:44:25 +0000

    3_2

--- Main.java
+++ Main.java
@@ -9,9 +9,10 @@ Scanner skan = new Scanner(System.in);
 Service1 s = new Service1();
 int x = 4;
 String imie;
+String nazwisko;
 int wiek;
-
-  
+String born;
+System.out.println("Witaj!");
 while(x!=0)
 {
 System.out.println("Wybierz funkcję: \n"+
@@ -26,13 +27,14 @@ case 1:
 Scanner skan1 = new Scanner(System.in);
 System.out.println("Podaj imie: ");
 imie=skan1.nextLine();
-
+System.out.println("Podaj nazwisko: ");
+nazwisko=skan1.nextLine();
 System.out.println("Podaj wiek: ");
 wiek=skan.nextInt();
-
-s.addStudent(new Student(imie, wiek));
+System.out.println("Podaj datę urodzenia: ");
+born=skan1.nextLine();
+s.addStudent(new Student(imie, nazwisko, wiek, born));
 }break;
-case 2:
 {
 var students = s.getStudents();
 for(Student current : students) {
